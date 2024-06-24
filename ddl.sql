c545c9a3a9e4970e56930a56c26e13f7b40cfa60
qpwisu <qpwisu@gmail.com>
2024-06-24 21:17:56 +0900

    feat : ddl delete cascade 제야조건 추가

--- ddl.sql
+++ ddl.sql
@@ -101,14 +101,14 @@ CREATE TABLE `theme_stock` (
 DROP TABLE IF EXISTS `stock_price_now`;
 
 CREATE TABLE `stock_price_now` (
-	`id`	BIGINT	NOT NULL,
+	`stock_id`	BIGINT	NOT NULL,
 	`date`	DATETIME(6)	NULL,
 	`open`	DECIMAL(10,2)	NULL,
 	`high`	DECIMAL(10,2)	NULL,
 	`low`	DECIMAL(10,2)	NULL,
 	`close`	DECIMAL(10,2)	NULL,
 	`volume`	BIGINT	NULL,
-	`change_rate`	DECIMAL(10,2)	NULL
+	`change_rate`	DECIMAL(10,2)
 );
 
 DROP TABLE IF EXISTS `theme_blog`;
@@ -199,6 +199,89 @@ CREATE TABLE `each_stock_aggregate` (
 	`theme_id`	BIGINT	NOT NULL
 );
 
+CREATE TABLE `date_stock_aggregate` (
+	`id`	bigint	NOT NULL,
+	`date`	date	NULL,
+	`cnt`	bigint	NULL,
+	`stock_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `date_theme_aggregate` (
+	`id`	bigint	NOT NULL,
+	`date`	date	NULL,
+	`cnt`	bigint	NULL,
+	`theme_id`	BIGINT	NOT NULL
+);
+CREATE TABLE `related_stock_aggregate` (
+	`id`	bigint	NOT NULL,
+	`search_stock_id`	BIGINT	NOT NULL,
+	`mention_stock_id`	BIGINT	NOT NULL,
+	`cnt`	INT	NOT NULL
+);
+
+CREATE TABLE `related_stock_aggregate` (
+	`id`	bigint	NOT NULL,
+	`search_stock_id`	BIGINT	NOT NULL,
+	`mention_stock_id`	BIGINT	NOT NULL,
+	`cnt`	INT	NOT NULL
+);
+CREATE TABLE `theme_mention_increments` (
+	`id`	bigint	NOT NULL,
+	`increase`	int	NOT NULL,
+	`theme_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `stock_mention_increments` (
+	`id`	bigint	NOT NULL,
+	`stock_id`	BIGINT	NOT NULL,
+	`increase`	int	NOT NULL
+);
+
+CREATE TABLE `favorite_theme` (
+	`id`	bigint	NOT NULL,
+	`user_id`	BIGINT	NOT NULL,
+	`theme_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `favorite_stock` (
+	`id`	bigint	NOT NULL,
+	`user_id`	BIGINT	NOT NULL,
+	`stock_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `theme_comment` (
+	`id`	bigint	NOT NULL,
+	`created_at`	DATETIME(6)	NULL,
+	`last_modified_at`	DATETIME(6)	NULL,
+	`body`	VARCHAR(255)	NULL,
+	`user_id`	BIGINT	NOT NULL,
+	`theme_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `stock_comment` (
+	`id`	bigint	NOT NULL,
+	`created_at`	DATETIME(6)	NULL,
+	`last_modified_at`	DATETIME(6)	NULL,
+	`body`	VARCHAR(255)	NULL,
+	`user_id`	BIGINT	NOT NULL,
+	`stock_id`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `theme_average_change_rate` (
+	`theme_id`	BIGINT	NOT NULL,
+	`average_change_rate`	DECIMAL(10,2)	NOT NULL,
+	`cnt`	int	NOT NULL
+);
+
+
+CREATE TABLE `suggest_month_stock` (
+	`id`	bigint	NOT NULL,
+	`cnt`	int	NOT NULL,
+	`month`	int	NOT NULL,
+	`stock_id`	BIGINT	NOT NULL
+);
+
+
 
 
 ALTER TABLE `comment` ADD CONSTRAINT `PK_COMMENT` PRIMARY KEY (
@@ -242,7 +325,7 @@ ALTER TABLE `theme_stock` ADD CONSTRAINT `PK_THEME_STOCK` PRIMARY KEY (
 );
 
 ALTER TABLE `stock_price_now` ADD CONSTRAINT `PK_stock_price_now` PRIMARY KEY (
-	`id`
+	`stock_id`
 );
 
 ALTER TABLE `theme_blog` ADD CONSTRAINT `PK_THEME_BLOG` PRIMARY KEY (
@@ -280,6 +363,50 @@ ALTER TABLE `each_stock_aggregate` ADD CONSTRAINT `PK_EACH_STOCK_AGGREGATE` PRIM
 );
 
 
+ALTER TABLE `date_stock_aggregate` ADD CONSTRAINT `PK_DATE_STOCK_AGGREGATE` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `date_theme_aggregate` ADD CONSTRAINT `PK_DATE_THEME_AGGREGATE` PRIMARY KEY (
+	`id`
+);
+ALTER TABLE `related_stock_aggregate` ADD CONSTRAINT `PK_RELATED_STOCK_AGGREGATE` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `theme_mention_increments` ADD CONSTRAINT `PK_THEMEMENTIONINCREMENTS` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `stock_mention_increments` ADD CONSTRAINT `PK_STOCKMENTIONINCREMENTS` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `favorite_theme` ADD CONSTRAINT `PK_FAVORITE_THEME` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `favorite_stock` ADD CONSTRAINT `PK_FAVORITE_STOCK` PRIMARY KEY (
+	`id`
+);
+ALTER TABLE `theme_comment` ADD CONSTRAINT `PK_THEME_COMMENT` PRIMARY KEY (
+	`id`
+);
+
+ALTER TABLE `stock_comment` ADD CONSTRAINT `PK_STOCK_COMMENT` PRIMARY KEY (
+	`id`
+);
+ALTER TABLE `theme_average_change_rate` ADD CONSTRAINT `PK_THEME_AVERAGE_CHANGE_RATE` PRIMARY KEY (
+	`theme_id`
+);
+
+ALTER TABLE `suggest_month_stock` ADD CONSTRAINT `PK_SUGGEST_MONTH_STOCK` PRIMARY KEY (
+	`id`
+);
+
+
+
+
 # 외래키 설정전에 auto_increment 설정 (erd_cloud에서 auto_increment 설정이 불가능해서 따로 처리해야함)
 ALTER TABLE user MODIFY COLUMN id bigint auto_increment  NOT NULL;
 ALTER TABLE comment  MODIFY COLUMN id bigint auto_increment  NOT NULL;
@@ -296,103 +423,115 @@ ALTER TABLE themes MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE stock_blog MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE stock_blog_stock_name MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE stock_price MODIFY COLUMN id bigint auto_increment NOT NULL;
+
 ALTER TABLE total_theme_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE total_stock_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE each_theme_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
 ALTER TABLE each_stock_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
 
+ALTER TABLE date_stock_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE date_theme_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
 
+ALTER TABLE related_stock_aggregate MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE theme_mention_increments MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE stock_mention_increments MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE favorite_theme MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE favorite_stock MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE theme_comment MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE stock_comment MODIFY COLUMN id bigint auto_increment NOT NULL;
+ALTER TABLE suggest_month_stock MODIFY COLUMN id bigint auto_increment NOT NULL;
 
 ALTER TABLE `comment` ADD CONSTRAINT `FK_board_TO_comment_1` FOREIGN KEY (
 	`board_id`
 )
 REFERENCES `board` (
 	`id`
-);
+) ON DELETE CASCADE;
 
 ALTER TABLE `comment` ADD CONSTRAINT `FK_user_TO_comment_1` FOREIGN KEY (
 	`user_id`
 )
 REFERENCES `user` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `board` ADD CONSTRAINT `FK_upload_image_TO_board_1` FOREIGN KEY (
 	`upload_image_id`
 )
 REFERENCES `upload_image` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `board` ADD CONSTRAINT `FK_user_TO_board_1` FOREIGN KEY (
 	`user_id`
 )
 REFERENCES `user` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `like` ADD CONSTRAINT `FK_board_TO_like_1` FOREIGN KEY (
 	`board_id`
 )
 REFERENCES `board` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `like` ADD CONSTRAINT `FK_user_TO_like_1` FOREIGN KEY (
 	`user_id`
 )
 REFERENCES `user` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `stock_info` ADD CONSTRAINT `FK_stocks_TO_stock_info_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `theme_stock` ADD CONSTRAINT `FK_themes_TO_theme_stock_1` FOREIGN KEY (
 	`theme_id`
 )
 REFERENCES `themes` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `theme_stock` ADD CONSTRAINT `FK_stocks_TO_theme_stock_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `stock_price_now` ADD CONSTRAINT `FK_stocks_TO_stock_price_now_1` FOREIGN KEY (
-	`id`
+	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
+
 
 ALTER TABLE `theme_blog` ADD CONSTRAINT `FK_themes_TO_theme_blog_1` FOREIGN KEY (
 	`theme_id`
 )
 REFERENCES `themes` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `theme_blog_stock_name` ADD CONSTRAINT `FK_theme_blog_TO_theme_blog_stock_name_1` FOREIGN KEY (
 	`theme_blog_id`
 )
 REFERENCES `theme_blog` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `theme_blog_stock_name` ADD CONSTRAINT `FK_stocks_TO_theme_blog_stock_name_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 
 ALTER TABLE `stock_blog` ADD CONSTRAINT `FK_stocks_TO_stock_blog_1` FOREIGN KEY (
@@ -400,7 +539,7 @@ ALTER TABLE `stock_blog` ADD CONSTRAINT `FK_stocks_TO_stock_blog_1` FOREIGN KEY 
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 
 ALTER TABLE `stock_blog_stock_name` ADD CONSTRAINT `FK_stock_blog_TO_stock_blog_stock_name_1` FOREIGN KEY (
@@ -408,21 +547,21 @@ ALTER TABLE `stock_blog_stock_name` ADD CONSTRAINT `FK_stock_blog_TO_stock_blog_
 )
 REFERENCES `stock_blog` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `stock_blog_stock_name` ADD CONSTRAINT `FK_stocks_TO_stock_blog_stock_name_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `stock_price` ADD CONSTRAINT `FK_stocks_TO_stock_price_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 
 ALTER TABLE `total_theme_aggregate` ADD CONSTRAINT `FK_themes_TO_total_theme_aggregate_1` FOREIGN KEY (
@@ -430,28 +569,28 @@ ALTER TABLE `total_theme_aggregate` ADD CONSTRAINT `FK_themes_TO_total_theme_agg
 )
 REFERENCES `themes` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `total_stock_aggregate` ADD CONSTRAINT `FK_stocks_TO_total_stock_aggregate_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `each_stock_aggregate` ADD CONSTRAINT `FK_stocks_TO_each_stock_aggregate_1` FOREIGN KEY (
 	`stock_id`
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `each_stock_aggregate` ADD CONSTRAINT `FK_themes_TO_each_stock_aggregate_1` FOREIGN KEY (
 	`theme_id`
 )
 REFERENCES `themes` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 
 
@@ -461,13 +600,115 @@ ALTER TABLE `each_theme_aggregate` ADD CONSTRAINT `FK_stocks_TO_each_theme_aggre
 )
 REFERENCES `stocks` (
 	`id`
-);
+)ON DELETE CASCADE;
 
 ALTER TABLE `each_theme_aggregate` ADD CONSTRAINT `FK_themes_TO_each_theme_aggregate_1` FOREIGN KEY (
 	`theme_id`
 )
 REFERENCES `themes` (
 	`id`
-);
+)ON DELETE CASCADE;
+ALTER TABLE `date_stock_aggregate` ADD CONSTRAINT `FK_stocks_TO_date_stock_aggregate_1` FOREIGN KEY (
+	`stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
 
+ALTER TABLE `date_theme_aggregate` ADD CONSTRAINT `FK_themes_TO_date_theme_aggregate_1` FOREIGN KEY (
+	`theme_id`
+)
+REFERENCES `themes` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `related_stock_aggregate` ADD CONSTRAINT `FK_stocks_TO_related_stock_aggregate_1` FOREIGN KEY (
+	`search_stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `related_stock_aggregate` ADD CONSTRAINT `FK_stocks_TO_related_stock_aggregate_2` FOREIGN KEY (
+	`mention_stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `theme_mention_increments` ADD CONSTRAINT `FK_themes_TO_themeMentionIncrements_1` FOREIGN KEY (
+	`theme_id`
+)
+REFERENCES `themes` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `stock_mention_increments` ADD CONSTRAINT `FK_stocks_TO_stockMentionIncrements_1` FOREIGN KEY (
+	`stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
+ALTER TABLE `favorite_theme` ADD CONSTRAINT `FK_user_TO_favorite_theme_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `user` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `favorite_theme` ADD CONSTRAINT `FK_themes_TO_favorite_theme_1` FOREIGN KEY (
+	`theme_id`
+)
+REFERENCES `themes` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `favorite_stock` ADD CONSTRAINT `FK_user_TO_favorite_stock_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `user` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `favorite_stock` ADD CONSTRAINT `FK_stocks_TO_favorite_stock_1` FOREIGN KEY (
+	`stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `theme_comment` ADD CONSTRAINT `FK_user_TO_theme_comment_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `user` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `theme_comment` ADD CONSTRAINT `FK_themes_TO_theme_comment_1` FOREIGN KEY (
+	`theme_id`
+)
+REFERENCES `themes` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `stock_comment` ADD CONSTRAINT `FK_user_TO_stock_comment_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `user` (
+	`id`
+)ON DELETE CASCADE;
+
+ALTER TABLE `stock_comment` ADD CONSTRAINT `FK_stocks_TO_stock_comment_1` FOREIGN KEY (
+	`stock_id`
+)
+REFERENCES `stocks` (
+	`id`
+)ON DELETE CASCADE;
+ALTER TABLE `theme_average_change_rate` ADD CONSTRAINT `FK_themes_TO_theme_average_change_rate_1` FOREIGN KEY (
+	`theme_id`
+)
+REFERENCES `themes` (
+	`id`
+)ON DELETE CASCADE;
 create index idx_ticker on stocks(ticker);
+
